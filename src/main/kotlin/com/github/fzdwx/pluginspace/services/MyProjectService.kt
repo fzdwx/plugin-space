ad41c47ecf59dc59e786dffacb07e2b9d1f74461
GitHub Action <action@github.com>
2022-08-25 11:22:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/fzdwx/pluginspace/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fzdwx/pluginspace/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fzdwx.pluginspace.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fzdwx.pluginspace.MyBundle
 
 class MyProjectService(project: Project) {
 
